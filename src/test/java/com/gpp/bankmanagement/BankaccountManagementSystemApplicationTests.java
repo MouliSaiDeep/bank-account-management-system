03e280e6a9f412ab886bcd363107eb10371884b3
Mouli Sai Deep <moulisaideep.get@gmail.com>
2026-03-22 19:48:00 +0530

    chore: initialize spring boot project scaffold

---
+++ src/test/java/com/gpp/bankmanagement/BankaccountManagementSystemApplicationTests.java
@@ -0,0 +1,14 @@
+package com.gpp.bankmanagement;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class BankaccountManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
